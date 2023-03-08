a110ca833330d1f1669517a0fa8feda719cd176d
eman-m0hammed <ma3920457@gmail.com>
2023-03-08 23:15:12 +0200

    creat api controller for each model

---
+++ app/Http/Controllers/API/JobQuestionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobQuestion;
+use Illuminate\Http\Request;
+
+class JobQuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobQuestion $jobQuestion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobQuestion $jobQuestion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JobQuestion $jobQuestion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobQuestion $jobQuestion)
+    {
+        //
+    }
+}
