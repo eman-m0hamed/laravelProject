9d907e806ffb1f1d3dcaeccccdc00308af74b0a0
eman-m0hammed <ma3920457@gmail.com>
2023-03-08 22:49:03 +0200

    edit user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'userName',
+        'phone',
+        'address',
+        'city',
+        'state',
     ];
 
     /**
