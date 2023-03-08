bb7459e9eca40926aa166a2f2dadd2568249c162
eman-m0hammed <ma3920457@gmail.com>
2023-03-08 22:43:52 +0200

    create job model

---
+++ app/Models/Job.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'details',
+        'state',
+        'open_date',
+        'close_date',
+    ];
+
+    public function Admin(){
+        return $this->belongsTo(Admin::class);
+    }
+
+    public function Question(){
+        return $this->hasMany(JobQuestion::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function jobsUser()
+    {
+        return $this->belongsToMany(User::class)
+        ->withPivot('answer_count', 'rightAnswer_count', 'status')
+        ->withTimestamps();
+    }
+}
