070c35b55dc64cc278b9b0fe433053e0b1dfa8b8
eman-m0hammed <ma3920457@gmail.com>
2023-03-08 22:43:19 +0200

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function Jobs(){
+        return $this->hasMany(Job::class, 'admin_id');
+    }
+}
+
+
