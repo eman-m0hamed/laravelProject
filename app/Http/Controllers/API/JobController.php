84a97c4ee9fa84bfd7866ab48f437c0a74fe2398
ayaatadel <ayaatadel128@gmail.com>
2023-03-18 01:24:21 +0200

    User Crud Operation

--- app/Http/Controllers/API/JobController.php
+++ app/Http/Controllers/API/JobController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -12,7 +13,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+       
     }
 
     /**
